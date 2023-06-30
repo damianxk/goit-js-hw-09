5f350e289eeb10fc1c95f7711c30074e6d9848f6
Damian Kowalczyk <damiank209@gmail.com>
2023-06-30 12:53:23 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+let color;
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const bgChange = () => {
+  color = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+};
+
+const stopBgChange = () => {
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+  clearInterval(color);
+};
+
+startBtn.addEventListener('click', bgChange);
+stopBtn.addEventListener('click', stopBgChange);
