2db0938186424389a1d7b0c9d23dc56c1f827524
Damian Kowalczyk <damiank209@gmail.com>
2023-07-11 16:28:12 +0200

    timer.js and promises.js
    Added timer and promises. Everything works fine

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,34 @@
+import Notiflix from 'notiflix';
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    if (shouldResolve) {
+      resolve(`👍 Fulfilled promise ${position} in ${delay}ms`);
+    } else {
+      reject(`👎 Rejected promise ${position} in ${delay}ms`);
+    }
+  });
 }
+
+const firstDelayEl = document.querySelector('[name="delay"]');
+const stepDelayEl = document.querySelector('[name="step"]');
+const amountEl = document.querySelector('[name="amount"]');
+const submitBtn = document.querySelector('[type="submit"]');
+
+const showPromise = (amount, step, time, current = 1) => {
+  if (current > amount) return;
+  createPromise(current, time)
+    .then(success => Notiflix.Notify.success(success))
+    .catch(error => Notiflix.Notify.failure(error));
+  time += step;
+  setTimeout(() => showPromise(amount, step, time, current + 1), step);
+};
+
+submitBtn.addEventListener('click', e => {
+  e.preventDefault();
+  setTimeout(
+    () => showPromise(+amountEl.value, +stepDelayEl.value, +firstDelayEl.value),
+    +firstDelayEl.value
+  );
+});
